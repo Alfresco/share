b7d264366da5a35e5242db10703778b61fe87243
Samuel Langlois <samuel.langlois@alfresco.com>
2015-04-30 16:20:56 +0000

    Merged HEAD-BUG-FIX (5.1) to trunk (5.1/Cloud40)    102938: QA-1669 Removed tests DetailsPageTest isPermissionsPanelPresent and FolderDetailsPageTest testFolderPermissions as PermissionPanel is no longer displayed on details page.

--- share-po/src/test/java/org/alfresco/po/share/site/document/DetailsPageTest.java
+++ share-po/src/test/java/org/alfresco/po/share/site/document/DetailsPageTest.java
@@ -258,14 +258,8 @@ public class DetailsPageTest extends AbstractTest
         Assert.assertTrue(folderDetails.isDocumentActionPresent(DocumentAction.DOWNLOAD_FOLDER), "Download Folder is not present");
 
     }    
-    
-    @Test(dependsOnMethods="isDocumentActionPresent", groups = { "Enterprise4.2" })
-    public void isPermissionsPanelPresent() throws Exception
-    {
-        Assert.assertTrue(folderDetails.isPermissionsPanelPresent());
-    }  
-    
-    @Test(dependsOnMethods="isPermissionsPanelPresent", groups = { "Hybrid" })
+
+    @Test(dependsOnMethods="isDocumentActionPresent", groups = { "Hybrid" })
     public void isSynPanelPresent() throws Exception
     {
         SitePage page = drone.getCurrentPage().render();
