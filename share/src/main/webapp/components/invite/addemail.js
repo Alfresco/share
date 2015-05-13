1daf7ae25b66b8628ef5f89166becfba857c1c8b
Tatyana Valkevych <tatyana.valkevych@alfresco.com>
2015-05-13 16:15:12 +0000

    Merged 5.0.N (5.0.2) to HEAD-BUG-FIX (5.1/Cloud)    104061: Merged V4.2-BUG-FIX (4.2.5) to 5.0.N (5.0.2)       104016: Merged NESS/4.2.N-2015_03_12 (4.2.5) to V4.2-BUG-FIX (4.2.5)          103820: MNT-14011 : No eMail address sanity check for external user site invitations             -added client validation to the inputs; added length limit to the inputs

--- share/src/main/webapp/components/invite/addemail.js
+++ share/src/main/webapp/components/invite/addemail.js
@@ -37,64 +37,81 @@
     * @return {Alfresco.AddEmailInvite} The new AddEmailInvite instance
     * @constructor
     */
-   Alfresco.AddEmailInvite = function(htmlId)
-   {
+    Alfresco.AddEmailInvite = function(htmlId)
+    {
       return Alfresco.AddEmailInvite.superclass.constructor.call(this, "Alfresco.AddEmailInvite", htmlId);
-   };
-   
-   YAHOO.extend(Alfresco.AddEmailInvite, Alfresco.component.Base,
-   {
-      /**
-       * Fired by YUI when parent element is available for scripting.
-       * Component initialisation, including instantiation of YUI widgets and event listener binding.
-       *
-       * @method onReady
-       */
-      onReady: function AddEmailInvite_onReady()
-      {  
-         // listen on ok button click
-         this.widgets.addEmailButton = Alfresco.util.createYUIButton(this, "add-email-button", this.addEmailButtonClick);         
-      },
+    };
 
-      /**
-       * Add email button click
-       *
-       * @method addEmailButtonClick
-       * @param e {object} DOM Event
-       * @param p_obj {object} Optional object literal from event listener definition
-       */
-      addEmailButtonClick: function AddEmailInvite_addEmailButtonClick(e, p_obj)
-      {
-         // fetch the firstname, lastname nad email
-         var firstNameElem = YAHOO.util.Dom.get(this.id + "-firstname"),
-            firstName = firstNameElem.value,
-            lastNameElem = YAHOO.util.Dom.get(this.id + "-lastname"),
-            lastName = lastNameElem.value,
-            emailElem = YAHOO.util.Dom.get(this.id + "-email"),
-            email = emailElem.value;
-         
-         // check whether we got enough information to proceed
-         if (firstName.length < 1 || lastName.length < 1 || email.length < 1)
-         {
-            Alfresco.util.PopupManager.displayMessage(
+    YAHOO.extend(Alfresco.AddEmailInvite, Alfresco.component.Base,
+    {      
+   /**
+    * Fired by YUI when parent element is available for scripting.
+    * Component initialisation, including instantiation of YUI widgets and event listener binding.
+    *
+    * @method onReady
+    */
+    onReady: function AddEmailInvite_onReady()
+    {  
+      // listen on ok button click
+      this.widgets.addEmailButton = Alfresco.util.createYUIButton(this, "add-email-button", this.addEmailButtonClick);
+
+      // Form definition
+      var form = new Alfresco.forms.Form(this.id + "-form");
+      this.widgets.form = form;
+      form.setSubmitElements(this.widgets.addEmailButton);         
+
+      // Form field validation
+      form.addValidation(this.id + "-firstname", Alfresco.forms.validation.mandatory, null, "keyup");
+      form.addValidation(this.id + "-lastname", Alfresco.forms.validation.mandatory, null, "keyup");
+      form.addValidation(this.id + "-email", Alfresco.forms.validation.mandatory, null, "keyup");           
+      form.addValidation(this.id + "-email", Alfresco.forms.validation.email, null, "change");
+
+      // Initialise the form
+      form.init();
+    },
+
+   /**
+    * Add email button click
+    *
+    * @method addEmailButtonClick
+    * @param e {object} DOM Event
+    * @param p_obj {object} Optional object literal from event listener definition
+   */
+    addEmailButtonClick: function AddEmailInvite_addEmailButtonClick(e, p_obj)
+    {
+      if(this.widgets.form.validate())
+        {
+           // fetch the firstname, lastname nad email
+           var firstNameElem = YAHOO.util.Dom.get(this.id + "-firstname"),
+               firstName = firstNameElem.value,
+               lastNameElem = YAHOO.util.Dom.get(this.id + "-lastname"),
+               lastName = lastNameElem.value,
+               emailElem = YAHOO.util.Dom.get(this.id + "-email"),
+               email = emailElem.value;
+           
+           // Fire the personSelected bubble event
+            YAHOO.Bubbling.fire("personSelected",
             {
-               text: this.msg("addemail.mandatoryfieldsmissing")
+               firstName: firstName,
+               lastName: lastName,
+               email: email
             });
-            return;
-         }
-         
-         // Fire the personSelected bubble event
-         YAHOO.Bubbling.fire("personSelected",
-         {
-            firstName: firstName,
-            lastName: lastName,
-            email: email
-         });
-            
-         // clear the values
-         firstNameElem.value = "";
-         lastNameElem.value = "";
-         emailElem.value = "";
+              
+           // clear the values
+           firstNameElem.value = "";
+           lastNameElem.value = "";
+           emailElem.value = "";
+        }
+        else
+        {
+           Alfresco.util.PopupManager.displayMessage(
+             {
+               text: this.msg("addemail.mandatoryfieldsmissing")
+             });
+           
+          return;       
+        }
       }
+
    });
 })();
