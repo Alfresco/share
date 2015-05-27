9f736f7bf156d12a1094e5a1925a70f583a3a5bb
Matt Ward <matt.ward@alfresco.com>
2015-05-27 17:27:51 +0000

    RA-262: Webscript to provide JSON view of /META-INF/MANIFEST.MF file.
    Tests, and about box changes yet to come. Webscript available on this URL:
    
        http://localhost:8081/share/service/share-manifest

---
+++ share/src/main/java/org/alfresco/web/scripts/ShareManifest.java
@@ -0,0 +1,122 @@
+/*
+ * Copyright (C) 2005-2015 Alfresco Software Limited.
+ *
+ * This file is part of Alfresco
+ *
+ * Alfresco is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Alfresco is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Alfresco. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.alfresco.web.scripts;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
+import org.json.simple.JSONObject;
+import org.springframework.core.io.Resource;
+import org.springframework.extensions.webscripts.AbstractWebScript;
+import org.springframework.extensions.webscripts.Container;
+import org.springframework.extensions.webscripts.Description;
+import org.springframework.extensions.webscripts.WebScriptRequest;
+import org.springframework.extensions.webscripts.WebScriptResponse;
+
+/**
+ * WebScript that provides a JSON view of a manifest file. The file is read once upon
+ * initialisation and the resulting JSON is stored for the life of the web app
+ * (the assumption is that this file will not change).
+ * 
+ * @author Matt Ward
+ */
+public class ShareManifest extends AbstractWebScript
+{
+    private final Resource resource; 
+    private Manifest manifest;
+    private String json;
+    
+    public ShareManifest(Resource resource)
+    {
+        if (resource == null)
+        {
+            throw new IllegalArgumentException("Manifest 'resource' parameter must not be null.");
+        }
+        this.resource = resource;
+    }
+    
+    @Override
+    public void init(Container container, Description description)
+    {
+        super.init(container, description);
+        readManifest();
+        manifestToJSON();
+    }
+
+    /**
+     * Read the manifest file that was specified in the constructor.
+     */
+    private void readManifest()
+    {
+        try (InputStream is = resource.getInputStream())
+        {
+            manifest = new Manifest(is);
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException("Error reading manifest.", e);
+        }
+    }
+
+    /**
+     * Convert the manifest to a JSON String.
+     */
+    private void manifestToJSON()
+    {
+        if (manifest == null)
+        {
+            throw new IllegalStateException("Manifest is null and must not be.");
+        }
+        Attributes attrs = manifest.getMainAttributes();
+        JSONObject jsonObject = new JSONObject(attrs);
+        json = jsonObject.toJSONString();
+    }
+    
+    @Override
+    public void execute(WebScriptRequest req, WebScriptResponse res) throws IOException
+    {
+        if (json == null)
+        {
+            throw new IllegalStateException("JSON respresentation of manifest is null.");
+        }
+        Writer w = null;
+        PrintWriter pw = null;
+        try
+        {
+            w = res.getWriter();
+            pw = new PrintWriter(w);
+            pw.print(json);
+        }
+        finally
+        {
+            if (pw != null)
+            {
+                pw.close();
+            }
+            if (w != null)
+            {
+                w.close();
+            }
+        }
+    }
+}
