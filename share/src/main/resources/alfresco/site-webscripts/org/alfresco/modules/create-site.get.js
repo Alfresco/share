9e906d0b5331b92b7877444c34e785ccbf9b0877
Ahmed Owian <ahmed.owian@alfresco.com>
2015-07-10 14:50:14 +0000

    SHA-1180: New Layout and Content for Create Site Dialog    - Conditionally showed site presets

--- share/src/main/resources/alfresco/site-webscripts/org/alfresco/modules/create-site.get.js
+++ share/src/main/resources/alfresco/site-webscripts/org/alfresco/modules/create-site.get.js
@@ -1,8 +1,8 @@
 var sitePresets = [{id: "site-dashboard", name: msg.get("title.collaborationSite")}];
 model.sitePresets = sitePresets;
 
-// Since there is only one sitePreset, showing it is irrelevant.
-// If adding a custom site type, change this class to "" so the Site Presets will show up on the Create Site form
-model.sitePresetsClass = " hidden";
+// When there is only one sitePreset, showing it is irrelevant.
+// If adding a custom site type, this class changes to "" so the Site Presets will show up on the Create Site form
+model.sitePresetsClass = (sitePresets.length == 1) ? " hidden" : "";
 
 model.defaultVisibility = "PUBLIC";
