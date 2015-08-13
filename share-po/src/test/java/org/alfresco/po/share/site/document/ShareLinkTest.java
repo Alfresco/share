a6242da4eb70518e14bb3c47c8d5049ef377d414
Michael Suzuki <michael.suzuki@alfresco.com>
2015-08-13 11:08:55 +0000

    tidy up, remove unused imports and remove property not used in pom

--- share-po/src/test/java/org/alfresco/po/share/site/document/ShareLinkTest.java
+++ share-po/src/test/java/org/alfresco/po/share/site/document/ShareLinkTest.java
@@ -18,7 +18,6 @@ import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
