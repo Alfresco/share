da3d5d4928a47a8079adc1f974421f764f76288e
Ahmed Owian <ahmed.owian@alfresco.com>
2015-09-02 16:28:32 +0000

    SHA-1391: Create Site dialog does not offer site type choice when RM amp is installed    - Delayed hiding site type until loading freemarker template in order to allow extensions such as RM to alter the sitePresets

--- share/src/main/resources/alfresco/site-webscripts/org/alfresco/modules/create-site.get.js
+++ share/src/main/resources/alfresco/site-webscripts/org/alfresco/modules/create-site.get.js
@@ -1,8 +1,4 @@
 var sitePresets = [{id: "site-dashboard", name: msg.get("title.collaborationSite")}];
 model.sitePresets = sitePresets;
 
-// When there is only one sitePreset, showing it is irrelevant.
-// If adding a custom site type, this class changes to "" so the Site Presets will show up on the Create Site form
-model.sitePresetsClass = (sitePresets.length == 1) ? " hidden" : "";
-
 model.defaultVisibility = "PUBLIC";
