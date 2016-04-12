c248c6ad00209812e85e71415b04ac4fe94bd31f
Alan Davis <alan.davis@alfresco.com>
2016-04-12 14:04:24 +0000

    MNT-16046: Merged 5.1.N (5.1.2) to 5.1.1 (5.1.1)    125181 adavis: Merged 5.0.N (5.0.4) to 5.1.N (5.1.2)       125165 adavis: Merged V4.2-BUG-FIX (4.2.7) to 5.0.N (5.0.4)          125158 rneamtu: Merged V4.2.6 (4.2.6) to V4.2-BUG-FIX (4.2.7)             125125 rneamtu: MNT-16014: 4.2.6 [Security] HTML injection in WCMQS site - Blog section                - Prevent for XSS attack stream asset in wcmqs

--- wcmquickstart-module/wcmquickstartwebsite/src/main/java/org/alfresco/wcm/client/directive/AssetDirective.java
+++ wcmquickstart-module/wcmquickstartwebsite/src/main/java/org/alfresco/wcm/client/directive/AssetDirective.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2005-2010 Alfresco Software Limited.
+ * Copyright (C) 2005-2016 Alfresco Software Limited.
  *
  * This file is part of the Alfresco Web Quick Start module.
  *
@@ -17,12 +17,18 @@
  */
 package org.alfresco.wcm.client.directive;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.Writer;
+import java.nio.charset.Charset;
 import java.util.Map;
 
 import org.alfresco.wcm.client.Asset;
 import org.alfresco.wcm.client.ContentStream;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.extensions.webscripts.ui.common.StringUtils;
 
 import freemarker.core.Environment;
 import freemarker.ext.beans.StringModel;
@@ -31,6 +37,7 @@ import freemarker.template.TemplateDirectiveModel;
 import freemarker.template.TemplateException;
 import freemarker.template.TemplateModel;
 import freemarker.template.TemplateModelException;
+import freemarker.template.utility.StringUtil;
 
 /**
  * Freemarker directive to stream a repository asset's content to the HTTP
@@ -41,10 +48,13 @@ import freemarker.template.TemplateModelException;
  */
 public class AssetDirective implements TemplateDirectiveModel
 {
+    protected static final int DEFAULT_BUFFERSIZE = 4096;
+    protected static final Log logger = LogFactory.getLog(AssetDirective.class);
+
     @SuppressWarnings("unchecked")
     @Override
-    public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body)
-            throws TemplateException, IOException
+    public void execute(Environment env, Map params, TemplateModel[] loopVars, TemplateDirectiveBody body) throws TemplateException,
+            IOException
     {
         if (params.size() != 1)
         {
@@ -59,11 +69,70 @@ public class AssetDirective implements TemplateDirectiveModel
         Asset asset = (Asset) assetParam.getWrappedObject();
 
         // Get the assets content stream
-        ContentStream stream = asset.getContentAsInputStream();
-
-        // Write the content stream to the servlet out
+        ContentStream contentStream = asset.getContentAsInputStream();
         Writer out = env.getOut();
-        stream.write(out);
+
+        String mimeType = contentStream.getMimeType();
+
+        String encoding = Charset.forName("UTF-8").name();
+        String content = null;
+
+        int bufferSize = DEFAULT_BUFFERSIZE;
+        int length = (int) contentStream.getLength();
+        if (length > 0 && length < bufferSize)
+        {
+            bufferSize = (int) length;
+        }
+
+        //MNT-16014: Parse and encode content in order to prevent XSS attacks
+        ByteArrayOutputStream bos = new ByteArrayOutputStream(bufferSize);
+        final InputStream input = contentStream.getStream();
+        if (input != null)
+        {
+            // get data into our byte buffer for processing
+            try
+            {
+                final byte[] buffer = new byte[bufferSize];
+                int read = input.read(buffer);
+                while (read != -1)
+                {
+                    bos.write(buffer, 0, read);
+                    read = input.read(buffer);
+                }
+            }
+            finally
+            {
+                input.close();
+            }
+
+            // convert to appropriate string format
+            content = encoding != null ? new String(bos.toByteArray(), encoding) : new String(bos.toByteArray());
+
+            // if found HTML content we need to process in-memory and perform stripping on
+            if (mimeType.contains("text/html") || mimeType.contains("application/xhtml+xml"))
+            {
+                // process with HTML stripper
+                content = StringUtils.stripUnsafeHTMLDocument(content, false);
+            }
+            else
+            {
+                // we cannot be sure what we are processing here - it could be
+                // html embedded in XML or anything else
+                content = StringUtil.XHTMLEnc(content);
+            }
+        }
+
+        // push the modified response to the real outputstream
+        try
+        {
+            // output the bytes
+            String processedContent = new String(content.getBytes(), encoding);
+            out.write(processedContent);
+        }
+        catch (IOException ex)
+        {
+            logger.error(ex);
+        }
     }
 
 }
