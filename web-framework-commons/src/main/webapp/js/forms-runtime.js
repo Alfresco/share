4e2d7fa442f74e66c86d2ee6015e4b3db82a0b15
Lucian Tuca <lucian.tuca1@ness.com>
2016-04-22 15:08:36 +0000

    MNT-16165 : ListOfValuesConstraint doesn't function properly when value has spaces at the end and/or start    - removed unnecessary trimming.

--- web-framework-commons/src/main/webapp/js/forms-runtime.js
+++ web-framework-commons/src/main/webapp/js/forms-runtime.js
@@ -1199,7 +1199,7 @@ Alfresco.forms.validation = Alfresco.forms.validation || {};
                {
                   name = element.id;
                }
-               var value = (element.type === "textarea") ? element.value : YAHOO.lang.trim(element.value);
+               var value = element.value;
                if (name)
                {
                   // check whether the input element is an array value
