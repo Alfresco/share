7b9b697186fe0ed4e4813cf7874e038132993c2d
Kevin Roast <kevin.roast@alfresco.com>
2016-08-08 14:57:29 +0000

    Merged 5.1.N (5.1.2) to 5.2.N (5.2.1)    129151 kroast: Merged 5.0.N (5.0.4) to 5.1.N (5.1.2) (PARTIAL MERGE)       129068 kroast: ESC-476, MNT-16162       Folder access in 'private' sites using site path url fails versus repository url path       SHA-1682       Granular Permissions File and Folder Access for non-site members       - Navigating to a Document Library folder link in the search results page to a folder you have been given access to in a private site. You can now navigate to the folder and all sub-folders you have been given access to within that private site.       - Navigating to a Document Details page link in the search results page to a document you have been given access to in a private site.        - The Tags 'Could not retrieve tags' and the Tree component "Site not found: ..." messages have been made less aggressive looking within the Document Library views - the red background has been removed.        - The private site missing title (was blank previously) in the title area has been replaced with the site url name which is all the user is able to resolve given the private permissions for the site object.       See tickets for more detail on fixes and what functionality is available.

--- share/src/main/resources/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js
+++ share/src/main/resources/alfresco/site-webscripts/org/alfresco/share/imports/share-header.lib.js
@@ -1113,7 +1113,7 @@ function getPageTitle() {
       var siteData = getSiteData();
       if (siteData != null)
       {
-         pageTitle = siteData.profile.title;
+         pageTitle = siteData.profile.title ? siteData.profile.title : page.url.templateArgs.site;
       }
    }
    else
