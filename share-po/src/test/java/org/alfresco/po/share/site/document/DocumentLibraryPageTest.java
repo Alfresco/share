6dda53ddab045c8726dcfbd7f7ce45b208a291d7
Meenal Bhave <meenal.bhave@alfresco.com>
2016-08-18 15:04:32 +0000

    Merged revision(s) 129524-129554 from web-apps/Share/trunk/share-po

--- share-po/src/test/java/org/alfresco/po/share/site/document/DocumentLibraryPageTest.java
+++ share-po/src/test/java/org/alfresco/po/share/site/document/DocumentLibraryPageTest.java
@@ -306,10 +306,10 @@ public class DocumentLibraryPageTest extends AbstractDocumentTest
     @Test(dependsOnMethods = "isContentUploadedSucessfulTest", groups = { "alfresco-one" })
     public void testGetPreviewUrl()
     {
-        String urlRegexp = "^http(s?)://((\\w+\\.)?\\w+\\.\\w+|((2[0-5]{2}|1[0-9]{2}|[0-9]{1,2})\\.){3}(2[0-5]{2}|1[0-9]{2}|[0-9]{1,2})):[0-9]{0,4}(/).+";
         documentLibPage = documentLibPage.getSiteNav().selectDocumentLibrary().render();
         FileDirectoryInfo fileDirectoryInfo = documentLibPage.getFileDirectoryInfo(file3.getName());
-        assertTrue(fileDirectoryInfo.getPreViewUrl().matches(urlRegexp));
+        String filePreviewUrl = fileDirectoryInfo.getPreViewUrl();
+        assertTrue(filePreviewUrl.startsWith(shareUrl),"URL Found: " + filePreviewUrl + "Share URL: " + shareUrl);
     }
 
     @Test(dependsOnMethods = "testGetPreviewUrl", groups = { "alfresco-one" })
