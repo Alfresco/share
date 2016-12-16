87da8ac04e3cf533bcb925000601a6643a31f744
Meenal Bhave <meenal.bhave@alfresco.com>
2016-12-16 16:56:11 +0000

    Improvements to address issues for PRR team (running tests on qa lab machines)

--- share-po/src/main/java/org/alfresco/po/share/SharePage.java
+++ share-po/src/main/java/org/alfresco/po/share/SharePage.java
@@ -776,9 +776,32 @@ public abstract class SharePage extends Page
     {
         return findAndWait(FOOTER_LOGO).getAttribute("src");
     }
+    
     public WebElement findByKey(final String id)
     {
         By criteria = By.id(getValue(id));
         return driver.findElement(criteria);
+    }
+    
+    /**
+     * Common method to wait for the next solr indexing cycle.
+     * 
+     * @param driver WebDriver Instance
+     * @param waitMiliSec Wait duration in milliseconds
+     */
+    public HtmlPage webDriverWait(long waitMiliSec)
+    {
+        synchronized (this)
+        {
+            try
+            {
+                this.wait(waitMiliSec);
+            }
+            catch (InterruptedException e)
+            {
+                // Discussed not to throw any exception
+            }
+        }
+        return this;
     }
 }
