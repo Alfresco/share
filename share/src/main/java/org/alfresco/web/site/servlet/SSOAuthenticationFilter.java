9d3a331645e709394d1e72cd783eb07781ec6609
ciprianchelaru <ciprian.chelaru.is@gmail.com>
2017-11-14 10:23:56 +0200

    removed duplicate code and added comments

--- share/src/main/java/org/alfresco/web/site/servlet/SSOAuthenticationFilter.java
+++ share/src/main/java/org/alfresco/web/site/servlet/SSOAuthenticationFilter.java
@@ -835,14 +835,14 @@ public class SSOAuthenticationFilter implements DependencyInjectedFilter, Callba
                 // If we are as yet unauthenticated but have external authentication, do a ping check as the external user.
                 // This will either establish the session or throw us out to log in as someone else!
                 userId = req.getRemoteUser();
+                
                 if(userId != null) {
-                    // Set the external auth flag so the UI knows we are using SSO etc.
-                    session.setAttribute(UserFactory.SESSION_ATTRIBUTE_EXTERNAL_AUTH, Boolean.TRUE);
                     if (logger.isDebugEnabled())
                         logger.debug("Initial login from externally authenticated user " + userId);
                     
                     setExternalAuthSession(session);
                 }else{
+                    // MNT-18402 : redirect to login page, when using SSO bypass link
                     redirectToLoginPage(req,res);
                 }
             }
