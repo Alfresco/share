db1f8cb5190a09113b86e8b35b095c43e1e3a8d5
Tuna Aksoy <tuna.aksoy@alfresco.com>
2017-12-07 17:53:06 +0000

    Addressed review comment

--- packaging/docker/substituter.sh
+++ packaging/docker/substituter.sh
@@ -9,7 +9,7 @@ if [[ $REPO_PORT == "" ]]; then
    REPO_PORT=8080
 fi
 
-echo "Replace with 'REPO_HOST' with '$REPO_HOST' and 'REPO_PORT' with '$REPO_PORT'"
+echo "Replace 'REPO_HOST' with '$REPO_HOST' and 'REPO_PORT' with '$REPO_PORT'"
 
 sed -i -e 's/REPO_HOST:REPO_PORT/'"$REPO_HOST:$REPO_PORT"'/g' /usr/local/tomcat/shared/classes/alfresco/web-extension/share-config-custom.xml
 
