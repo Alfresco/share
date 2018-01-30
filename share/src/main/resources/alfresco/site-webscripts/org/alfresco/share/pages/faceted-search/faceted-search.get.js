11b527b1788a0418031d459781841189adf782b1
David Webster <david@cognite.net>
2018-01-30 17:16:37 +0000

    MNT-18879: Fix config to ensure correct sort order toggle status at init

--- share/src/main/resources/alfresco/site-webscripts/org/alfresco/share/pages/faceted-search/faceted-search.get.js
+++ share/src/main/resources/alfresco/site-webscripts/org/alfresco/share/pages/faceted-search/faceted-search.get.js
@@ -511,7 +511,9 @@ var searchResultsMenuBar = {
                      name: "alfresco/menus/AlfMenuBarToggle",
                      config: {
                         visibilityConfig: hideOnNotSortableConfig,
+                        // subscriptionAttribute and hashName need to match. MNT-18879
                         hashName: "sortAscending",
+                        subscriptionAttribute: "sortAscending",
                         checkedValue: "true",
                         checked: true,
                         onConfig: {
