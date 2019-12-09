a8f840a3968df53cd53e2c3ce7b1570c0b9bba41
Andrei Forascu <andrei.forascu@ness.com>
2019-12-09 11:51:38 +0200

    MNT-20871: - cherry-pick bd6bb0d214b2c65ea61773ddbbeb9819690de159 from master

--- web-framework-commons/src/main/webapp/js/alfresco.js
+++ web-framework-commons/src/main/webapp/js/alfresco.js
@@ -304,6 +304,8 @@ Alfresco.util.onlineEditUrlAos = function(aos, record)
    var pathInRepository = record.webdavUrl.substring(7);
    // Construct a URL from the path in the repo
    var result = Alfresco.util.combinePaths(aos.baseUrl, pathInRepository);
+   // Get the first directory/entity from path (fix for MNT-20871)
+   var firstDir = pathInRepository.substring(1, pathInRepository.substring(1).indexOf("/") + 1);
    // Check if the length of the encoded path exceeds 256 characters. If so, use the nodeid instead of the path
    if (encodeURI(result).length > 256)
    {
@@ -313,13 +315,13 @@ Alfresco.util.onlineEditUrlAos = function(aos, record)
          throw new Error("Alfresco.util.onlineEditUrlAos: Missing filename in webdav URL.");
       }
       var filename = record.webdavUrl.substring(filenameSepIdx + 1);
-      result = Alfresco.util.combinePaths(aos.baseUrl, "_aos_nodeid", record.nodeRef.split("/").pop(), filename)
+      result = Alfresco.util.combinePaths(aos.baseUrl, firstDir, "_aos_nodeid", record.nodeRef.split("/").pop(), filename);
    }
    // If URL still exceeds 256 characters, we also need to replace the filename
    if(encodeURI(result).length > 256)
    {
       var fileextSepIdx = record.webdavUrl.lastIndexOf(".");
-      result = Alfresco.util.combinePaths(aos.baseUrl, "_aos_nodeid", record.nodeRef.split("/").pop(), "Document" + (fileextSepIdx > 0 ? record.webdavUrl.substring(fileextSepIdx) : ''));
+      result = Alfresco.util.combinePaths(aos.baseUrl, firstDir, "_aos_nodeid", record.nodeRef.split("/").pop(), "Document" + (fileextSepIdx > 0 ? record.webdavUrl.substring(fileextSepIdx) : ''));
    }
    return result;
 };
