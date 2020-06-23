5405e74710bb9d291725b7585c3195488e56582a
tiagosalvado10 <9038083+tiagosalvado10@users.noreply.github.com>
2020-06-23 13:28:46 +0100

    MNT-18448 MNT-19466: Reverting RTF and Visio extensions (#240)

--- share/src/main/webapp/components/documentlibrary/actions.js
+++ share/src/main/webapp/components/documentlibrary/actions.js
@@ -657,8 +657,6 @@
          "application/vnd.ms-word.document.macroenabled.12": "Word.Document",
          "application/vnd.openxmlformats-officedocument.wordprocessingml.template": "Word.Document",
          "application/vnd.ms-word.template.macroenabled.12": "Word.Document",
-         "application/rtf":"Word.Document",
-
 
          "application/vnd.ms-powerpoint": "PowerPoint.Slide",
          "application/vnd.openxmlformats-officedocument.presentationml.presentation": "PowerPoint.Slide",
@@ -679,13 +677,7 @@
          "application/vnd.ms-excel.addin.macroenabled.12": "Excel.Sheet",
          "application/vnd.ms-excel.sheet.binary.macroenabled.12": "Excel.Sheet",
          "application/vnd.visio": "Visio.Drawing",
-         "application/vnd.visio2013": "Visio.Drawing",
-         "application/vnd.ms-visio.drawing.macroenabled.main+xml": "Visio.Drawing",
-         "application/vnd.ms-visio.drawing.main+xml": "Visio.Drawing",
-         "application/vnd.ms-visio.stencil.macroenabled.main+xml": "Visio.Drawing",
-         "application/vnd.ms-visio.stencil.main+xml": "Visio.Drawing",
-         "application/vnd.ms-visio.template.macroenabled.main+xml": "Visio.Drawing",
-         "application/vnd.ms-visio.template.main+xml": "Visio.Drawing"
+         "application/vnd.visio2013": "Visio.Drawing"
       },
 
       /**
@@ -801,18 +793,7 @@
                docm: "application/vnd.ms-word.document.macroenabled.12",
                dotx: "application/vnd.openxmlformats-officedocument.wordprocessingml.template",
                dotm: "application/vnd.ms-word.template.macroenabled.12",
-               rtf: "application/rtf",
               
-               vsd: "application/vnd.visio",
-               vss: "application/vnd.visio",
-               vst: "application/vnd.visio",
-               vsdx: "application/vnd.ms-visio.drawing.main+xml",
-               vsdm: "application/vnd.ms-visio.drawing.macroenabled.main+xml",
-               vssx: "application/vnd.ms-visio.stencil.main+xml",
-               vssm: "application/vnd.ms-visio.stencil.macroenabled.main+xml",
-               vstx: "application/vnd.ms-visio.template.main+xml",
-               vstm: "application/vnd.ms-visio.template.macroenabled.main+xml",
-
                ppt: "application/vnd.ms-powerpoint",
                pptx: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
                pptm: "application/vnd.ms-powerpoint.presentation.macroenabled.12",
@@ -920,7 +901,6 @@
             'dot'  : 'ms-word',
             'dotx' : 'ms-word',
             'dotm' : 'ms-word',
-            'rtf'  : 'ms-word',
             'xls'  : 'ms-excel',
             'xlsx' : 'ms-excel',
             'xlsb' : 'ms-excel',
@@ -1286,18 +1266,6 @@
             'dot'  : 'ms-word',
             'dotx' : 'ms-word',
             'dotm' : 'ms-word',
-            'rtf'  : 'ms-word',
-           
-            'vsd': 'ms-visio',
-            'vss': 'ms-visio',
-            'vst': 'ms-visio',
-            'vsdx': 'ms-visio',
-            'vsdm': 'ms-visio',
-            'vssx': 'ms-visio',
-            'vssm': 'ms-visio',
-            'vstx': 'ms-visio',
-            'vstm': 'ms-visio',
-
             'xls'  : 'ms-excel',
             'xlsx' : 'ms-excel',
             'xlsb' : 'ms-excel',
