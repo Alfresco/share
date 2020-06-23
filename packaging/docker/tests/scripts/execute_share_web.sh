323b44a8203f2b792b843f96c6b16d4845c7879f
Claudia Agache <claudia.agache@ness.com>
2020-06-23 16:47:30 +0300

    removed not needed arguments

--- packaging/docker/tests/scripts/execute_share_web.sh
+++ packaging/docker/tests/scripts/execute_share_web.sh
@@ -3,7 +3,7 @@
 export GROUP=$1
 
 mvn clean install \
-               -DsuiteXmlFile="src/test/resources/share-po-runner-suite.xml" \
+               -DsuiteXmlFile=src/test/resources/share-po-runner-suite.xml \
                -Dskip.automationtests=false \
                -Dalfresco.restApi.basicAuthScheme=true \
                -Djmx.useJolokiaAgent=true \
@@ -11,22 +11,11 @@ mvn clean install \
                -DexcludeGroups="google-docs,unit,SmartFolders,ExternalUsers,tobefixed,office,TransformationServer,xsstests" \
                -DrunBugs=false \
                -Dalfresco.server=localhost \
-               -Dalfresco.host=localhost \
                -Dalfresco.port=8080 \
-               -Dalfresco.url="http://localhost:8080/alfresco" \
                -Dshare.host=localhost \
                -Dshare.port=8181 \
-               -Dshare.url="http://localhost:8181/share" \
-               -Dadmin.user=admin \
-               -Dadmin.password=admin \
-               -Dwebdriver.grid.url="http://127.0.0.1:4444/wd/hub" \
-               -Dwebdriver.local.grid=false \
-               -Dwebdriver.localGrid=false \
-               -Dwebdriver.element.wait.time=30000 \
-               -Dwebdriver.element.wait.time=20000 \
-               -Dwebdriver.page.render.wait.time=60000 \ 
                -Dbrowser.name=Firefox \
-               -Dbrowser.version=44.0 \
+               -Dbrowser.version=76.0 \
                -DtestManagement.enabled=false \
                -DtestManagement.testRun=test \
                -DtestManagement.apiKey=6z8dAK8oYhO89N36iDtB-Lwmp2k8.aDmFfXLeR90o \
