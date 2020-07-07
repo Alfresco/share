22e8dc09ddc1d73c605e83a74471e333e8d6b78e
Andreea Nechifor <Andreea.Nechifor@ness.com>
2020-07-07 15:02:03 +0300

    APPS-228: change build script

---
+++ _ci/build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "=========================== Starting Build&Test Script ==========================="
+PS4="\[\e[35m\]+ \[\e[m\]"
+set -vex
+pushd "$(dirname "${BASH_SOURCE[0]}")/../"
+
+# Change tag if you are on a branch
+if [ ! -z "$TRAVIS_BRANCH" -a "$TRAVIS_BRANCH" != "master" ]; then
+  sed  -i "s/<image.tag>latest/<image.tag>latest-$TRAVIS_BRANCH/" packaging/docker/pom.xml
+fi
+
+mvn -B -U \
+    clean install \
+    -Dbuildnumber=${TRAVIS_BUILD_NUMBER} \
+    "-Plocal"
+
+cd packaging/docker
+mvn fabric8:push
+
+popd
+set +vex
+echo "=========================== Finishing Build&Test Script =========================="
