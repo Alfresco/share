830140df1e9605a70865da17d73ca1a1a1f994b4
Andreea Nechifor <Andreea.Nechifor@ness.com>
2020-07-07 18:06:50 +0300

    APPS-228: added image tag name.

--- _ci/build.sh
+++ _ci/build.sh
@@ -5,11 +5,12 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
+# substitude all '/' to '-' as Docker doesn't allow it
 TAG_NAME=`echo $TRAVIS_BRANCH | tr / - `
 
 # Change tag if you are on a branch
 if [ ! -z "$TRAVIS_BRANCH" -a "$TRAVIS_BRANCH" != "master" ]; then
-  sed  -i '/<image.tag>latest/<image.tag>latest-$TAG_NAME/' packaging/docker/pom.xml
+  sed  -i "s/<image.tag>latest/<image.tag>latest-$TAG_NAME/" packaging/docker/pom.xml
 fi
 
 mvn -B -U \
