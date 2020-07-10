aee6bcc7a6d12758f74438ba2048b3c034508ffa
Andreea Nechifor <Andreea.Nechifor@ness.com>
2020-07-10 13:01:56 +0300

    added time for scripts

--- _ci/init.sh
+++ _ci/init.sh
@@ -9,10 +9,6 @@ mkdir -p ${HOME}/.m2 && cp -rf _ci/settings.xml ${HOME}/.m2/
 echo "${QUAY_PASSWORD}" | docker login -u="alfresco+bamboo" --password-stdin quay.io
 find "${HOME}/.m2/repository/" -type d -name "*-SNAPSHOT*" | xargs -r -l rm -rf
 
-# Enable experimental docker features (e.g. squash options)
-echo '{"experimental":true}' | sudo tee /etc/docker/daemon.json
-sudo service docker restart
-
 popd
 set +vex
 echo "=========================== Finishing Init Script =========================="
