a11f0dd458239f60d7ddee94e41f75c446b0aace
Andreea Nechifor <Andreea.Nechifor@ness.com>
2020-07-10 11:46:52 +0300

    APPS-228: added build

--- _ci/build.sh
+++ _ci/build.sh
@@ -5,15 +5,21 @@ PS4="\[\e[35m\]+ \[\e[m\]"
 set -vex
 pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
-source _ci//init_tag.sh
+source _ci/init_tag.sh
 
 # Change tag if you are on a branch
 if [ ! -z "$TRAVIS_BRANCH" -a "$TRAVIS_BRANCH" != "master" ]; then
   sed  -i "s/<image.tag>latest/<image.tag>$TAG_NAME/" packaging/docker/pom.xml
 fi
 
-cd packaging/docker
+#build share
+mvn -B -q clean install \
+    -DskipTests \
+    -Dmaven.javadoc.skip=true 
+  
 
+#build image
+cd packaging/docker
 mvn install -Plocal 
 mvn fabric8:push
 
