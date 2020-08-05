1803a304711e7a97fea5781481fcaa89b41d627e
Andreea Nechifor <Andreea.Nechifor@ness.com>
2020-08-05 11:41:44 +0300

    added release version

--- _ci/init_tag.sh
+++ _ci/init_tag.sh
@@ -7,13 +7,19 @@ echo "The branch is ${TRAVIS_BRANCH}"
 echo "Project version: ${PROJECT_VERSION}"
 
 if [ "${TRAVIS_BRANCH}" = "master" ]; then
-  TAG_NAME=$RELEASE_VERSION
+  TAG_NAME=$latest
 else
   # substitude all '/' to '-' as Docker doesn't allow it
   TAG_NAME=`echo ${TRAVIS_BRANCH} | tr / - `
   TAG_NAME=${TAG_NAME}-${PROJECT_VERSION}
 fi
 
+#in case of release 
+if [ "${TRAVIS_COMMIT_MESSAGE}" = "trigger release" ]; then
+  TAG_NAME=$RELEASE_VERSION
+fi
+
 echo "Saving tag name as ${TAG_NAME}"
 
 echo "=========================== Ending Init Tag =========================="
+
