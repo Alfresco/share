f7f5d4fbe429478893d17bd6a6758c5e1e6ec5b0
Andreea Nechifor <Andreea.Nechifor@ness.com>
2020-08-05 15:48:07 +0300

    added release check

--- _ci/init_tag.sh
+++ _ci/init_tag.sh
@@ -13,11 +13,12 @@ else
   TAG_NAME=`echo ${TRAVIS_BRANCH} | tr / - `
   TAG_NAME=${TAG_NAME}-${PROJECT_VERSION}
 fi
-
 #in case of release 
-if [ "${TRAVIS_COMMIT_MESSAGE}" = *release* ]; then
-  TAG_NAME=$RELEASE_VERSION
+if [[ ${TRAVIS_COMMIT_MESSAGE} = *"release"* ]]; then
+    echo "Release version: ${RELEASE_VERSION}"
+    TAG_NAME=$RELEASE_VERSION
 fi
+
 
 echo "Saving tag name as ${TAG_NAME}"
 
