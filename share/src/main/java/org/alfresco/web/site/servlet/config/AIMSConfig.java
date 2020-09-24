9a3942e37da8953b46b9d8289706321f720bd272
Bogdan Ioan Gabor <Bogdan.Gabor@Ness.com>
2020-09-24 13:45:01 +0300

    APPS-480: Changed org.apache.myfaces.core:myfaces-impl library from 1.1.10 to 1.2.12 in order to solve commons-collections:commons-collections vulnerabilities pointed by WhiteSource (3.1 to 3.2)

--- share/src/main/java/org/alfresco/web/site/servlet/config/AIMSConfig.java
+++ share/src/main/java/org/alfresco/web/site/servlet/config/AIMSConfig.java
@@ -20,7 +20,7 @@
  */
 package org.alfresco.web.site.servlet.config;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.keycloak.common.enums.SslRequired;
