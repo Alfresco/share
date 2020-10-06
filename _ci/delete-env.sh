0abfb235bcd10da06c609115d6f6d5a0c5e39b53
Cristina Sirbu <cristina.sirbu@ness.com>
2020-10-06 16:35:34 +0300

    APPS-422: SHARE - Pipeline improvement with K8s and AWS Add get_route53_json function into delete script

--- _ci/delete-env.sh
+++ _ci/delete-env.sh
@@ -1,6 +1,30 @@
 export RELEASE_NAME=$NAMESPACE
 export RELEASE_INGRESS_NAME="${NAMESPACE}-ingress"
 
+# TODO: move this into a common_tools script and import it here
+#
+# Returns the Route53 json payload ChangeResourceRecordSets API call
+# Can take one argument that specifies the action: "CREATE", "DELETE", "UPSERT"; if none
+# specified, it defaults to "CREATE"
+#
+function get_route53_json {
+  local out
+  local action
+  local path="_ci/route53.json"
+
+  if [[ ( -z "$1" ) || ( "$1" != "CREATE" && "$1" != "DELETE" && "$1" != "UPSERT" ) ]]; then
+    action="CREATE"
+  else
+    action=$1
+  fi
+
+  out=$(sed -e "s/\$Action/$action/" \
+    -e "s/\$Name/${HOST}/" \
+    -e "s/\$Value/${ELBADDRESS}/" $path)
+
+  echo $out
+}
+
 #
 # Deletes the environment
 #
