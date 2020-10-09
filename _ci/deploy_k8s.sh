261af6e58ab324f8aac5c3786d5ef574ccc31be1
Cristina Sirbu <cristina.sirbu@ness.com>
2020-10-09 13:46:17 +0300

    APPS-422: SHARE - Pipeline improvement with K8s and AWS Move wait for pods script to make the build faster and fix update develop env function

--- _ci/deploy_k8s.sh
+++ _ci/deploy_k8s.sh
@@ -30,8 +30,6 @@ function isDevelopUp() {
 function updateDevelopEnv()  {
   echo "Update develop"
 
-  helm uninstall $RELEASE_NAME --namespace $NAMESPACE
-
   # add the helm repos
   helm repo add alfresco-incubator https://kubernetes-charts.alfresco.com/incubator
   helm repo add alfresco-stable https://kubernetes-charts.alfresco.com/stable
@@ -157,6 +155,49 @@ function createEnv {
   aws route53 change-resource-record-sets --hosted-zone-id $HOSTED_ZONE_ID --change-batch "$(get_route53_json  "UPSERT")"
 }
 
+#
+# Before running the tests make sure that all pods are green
+#
+function wait_for_pods {
+  # counters
+  PODS_COUNTER=0
+  # counters limit
+  PODS_COUNTER_MAX=120
+  # sleep seconds
+  PODS_SLEEP_SECONDS=10
+
+  namespace=$1
+
+  echo "Validate that all the pods in the deployment are ready"
+  while [ "$PODS_COUNTER" -lt "$PODS_COUNTER_MAX" ]; do
+    pendingpodcount=$(kubectl get pods --namespace "$namespace" | awk '{print $2}' | grep -c '0/1' || true)
+    if [ "$pendingpodcount" -eq 0 ]; then
+      runningPods=$(kubectl get pods --namespace "$namespace")
+      echo "All pods are Running and Ready!"
+      echo "$runningPods"
+      break
+    fi
+    PODS_COUNTER=$((PODS_COUNTER + 1))
+    echo "$pendingpodcount pods are not yet ready - sleeping $PODS_SLEEP_SECONDS seconds - counter $PODS_COUNTER"
+    sleep "$PODS_SLEEP_SECONDS"
+    continue
+  done
+  if [ "$PODS_COUNTER" -ge "$PODS_COUNTER_MAX" ]; then
+    failedPods=$(kubectl get pods --namespace "$namespace" | grep '0/1' | awk '{print $1}')
+    echo "\nThe following pods were not ready:\n"
+    for failedpod in $failedPods; do
+      echo "$failedpod"
+    done
+    for failedpod in $failedPods; do
+      echo "Description for $failedpod :\n"
+      kubectl describe pod $failedpod --namespace "$namespace"
+    done
+    echo "Pods did not start - exit"
+    kubectl get pods --namespace "$namespace"
+    exit 1
+  fi
+}
+
 # Main
 if $(isBranchDevelop); then
   echo "On branch develop"
@@ -164,11 +205,14 @@ if $(isBranchDevelop); then
   if $(isDevelopUp); then
     echo "Update develop environment"
     updateDevelopEnv
+    wait_for_pods $NAMESPACE
   else
     echo "Create develop environment"
     createEnv
+    wait_for_pods $NAMESPACE
   fi
 else
   echo "On development branch"
   createEnv
+  wait_for_pods $NAMESPACE
 fi
