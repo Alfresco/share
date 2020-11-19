16d518aba5f98baa35b88d6a7265aaf462486161
Gheorghiu Flavian <gheorghiu.flavian@yahoo.com>
2020-11-19 14:39:44 +0200

    APPS-578: use older nginx helm chart version

--- _ci/deploy_k8s.sh
+++ _ci/deploy_k8s.sh
@@ -120,7 +120,7 @@ function createEnv {
   kubectl apply -f _ci/values-for-ingress-travis-env.yaml
 
   # install ingress
-  helm upgrade --install $RELEASE_INGRESS_NAME ingress-nginx/ingress-nginx \
+  helm upgrade --install $RELEASE_INGRESS_NAME ingress-nginx/ingress-nginx --version 2.13.0 \
           --set controller.scope.enabled=true \
           --set controller.scope.namespace=$NAMESPACE \
           --set rbac.create=true \
