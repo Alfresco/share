fd8b05d8f3303f243cbc6f12d768e4c6ee364236
Piero Cittadino <63122825+pieCit87@users.noreply.github.com>
2020-11-26 12:06:06 +0200

    reverted the changes

--- share/src/main/resources/alfresco/site-webscripts/org/alfresco/components/documentlibrary/data/surf-doclist.get.js
+++ share/src/main/resources/alfresco/site-webscripts/org/alfresco/components/documentlibrary/data/surf-doclist.get.js
@@ -1,3 +1,3 @@
 <import resource="classpath:/alfresco/site-webscripts/org/alfresco/components/documentlibrary/data/surf-doclist.lib.js">
 
-surfDoclist_main(args["includeThumbnails"] == "true", stripLinkedNodeProperties);
+surfDoclist_main(args["includeThumbnails"] == "true");
