e6a389b7f10bf1501c64768dfefd0c912fdda382
Bogdan B <bogdan.bocancea@gmail.com>
2021-01-18 10:13:29 +0200

    - update test stage to run on chrome stable - update ACS to version 7.0.0-A22

--- _ci/execute_share_web.sh
+++ _ci/execute_share_web.sh
@@ -7,7 +7,7 @@ pushd "$(dirname "${BASH_SOURCE[0]}")/../"
 
 git clone https://${GIT_USERNAME}:${GIT_PASSWORD}@github.com/Alfresco/alfresco-tas-share-test.git
 cd alfresco-tas-share-test
-git checkout master
+git checkout feature/APPS-webdriver-manager-3
 
 if [[ "$TRAVIS_BRANCH" = "develop" ]]; then
   NAMESPACE="develop-share"
@@ -20,9 +20,7 @@ export HOST="${NAMESPACE}.${HOSTED_ZONE}"
 export XML_SUITE=$1
 mvn install \
                -DsuiteXmlFile="src/test/resources/test-suites/${XML_SUITE}" \
-               -Djmx.useJolokiaAgent=true \
                -DexcludeGroups='google-docs,unit,SmartFolders,ExternalUsers,tobefixed,office,TransformationServer,xsstests' \
-               -DrunBugs=false \
                -Dalfresco.scheme=https \
                -Dalfresco.server=$HOST \
                -Dalfresco.port=443 \
@@ -31,11 +29,7 @@ mvn install \
                -Dshare.url="https://$HOST/share" \
                -Dadmin.user=admin \
                -Dadmin.password=$ALF_PASSWORD \
-               -Dbrowser.headless=true \
-               -Dwebdriver.grid.url='http://127.0.0.1:4444/wd/hub' \
-               -Dbrowser.name=Firefox \
-               -Dbrowser.version=44.0 \
-               -Ddisplay.xport=99.0 \
+               -Dbrowser.name=chrome \
                -Daims.enabled=false & # send the long living command to background!
 
 minutes=0
