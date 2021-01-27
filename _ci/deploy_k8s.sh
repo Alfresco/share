95b76e497ea0cf8044362c8e3a1118cc9334c611
Piero Cittadino <63122825+pieCit87@users.noreply.github.com>
2021-01-27 21:00:04 +0200

    synservice

--- _ci/deploy_k8s.sh
+++ _ci/deploy_k8s.sh
@@ -64,7 +64,7 @@ function updateDevelopEnv()  {
           --set repository.image.tag="${REPO_TAG_NAME}" \
           --set share.image.repository="quay.io/alfresco/${ALFRESCO_SHARE_IMAGE}" \
           --set share.image.tag="${SHARE_TAG_NAME}" \
-          --set alfresco-sync-service.syncservice.tag="3.4.0-M2" \
+          --set alfresco-sync-service.syncservice.image.tag="3.4.0-M2" \
           --set repository.environment.JAVA_OPTS="-Dalfresco.restApi.basicAuthScheme=true -Dsolr.base.url=/solr -Dsolr.secureComms=none -Dindex.subsystem.name=solr6 -Dalfresco.cluster.enabled=true -Ddeployment.method=HELM_CHART -Dtransform.service.enabled=true -Xms2000M -Xmx2000M" \
           --namespace $NAMESPACE
 }
