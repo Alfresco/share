715b6442e901df7bcccea2660f9b59c9bfef94ba
Adina Parpalita <Adina.Parpalita@ness.com>
2021-03-04 15:21:13 +0200

    change color code

--- share-po/src/test/java/org/alfresco/po/AbstractTest.java
+++ share-po/src/test/java/org/alfresco/po/AbstractTest.java
@@ -136,8 +136,8 @@ public abstract class AbstractTest extends AbstractTestNGSpringContextTests impl
     public static long count = 0;
 
     public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_RED = "\u001B[91m";
-    public static final String ANSI_GREEN = "\u001B[92m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
     public static final String ANSI_BOLD = "\u001B[1m";
 
     @BeforeClass(alwaysRun = true)
